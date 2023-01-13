391a049dff031b12ca5b2418574a9cde62f19c7f
OmerTurkenits <omertur06@gmail.com>
2023-01-13 19:17:19 +0200

    Added Reader interface and classes

---
+++ SudokuProject/SudokuProject/Readers/CLIReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{ 
+    //A Command Line Reader implementation:
+    public class CLIReader : Reader
+    {
+        public override byte[,] read()
+        {
+            String input;
+            Console.WriteLine("Input From Keyboard:");
+            input = Console.ReadLine();
+
+            //Calls the convert function on the input.
+            return convert(input);
+        }
+    }
+}
---
+++ SudokuProject/SudokuProject/Readers/FileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProject
+{
+    //A File Reader implementation:
+    public class FileReader : Reader
+    {
+        public override byte[,] read()
+        {
+            
+            Console.WriteLine("1. C:\\Users\\Asus\\Desktop\\Omega-Sudoku\\SudokuProject\\SudokuProject\\sudoku.txt\n");
+            Console.Write("Enter a file path: ");
+
+            //Enter a file path.
+            string filePath = Console.ReadLine();
+
+            //Read from file.
+            string input = System.IO.File.ReadAllText($@"{filePath}");
+
+            //Calls the convert function on the input.
+            return convert(input);
+        }
+    }
+}
